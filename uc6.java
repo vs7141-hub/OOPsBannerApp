94e3c0a33d6d5379d279841e78c37e18132ab8f0
vs7141-hub <vs7141@srmist.edu.in>
2026-02-23 21:42:09 +0530

    ALL week 1 and 2 questions along with all the levels are completed here

---
+++ uc6.java
@@ -0,0 +1,49 @@
+public class BannerApp {
+
+    public static void main(String[] args) {
+        String[] o1 = getOPattern();
+        String[] o2 = getOPattern();
+        String[] p = getPPattern();
+        String[] s = getSPattern();
+
+        for (int i = 0; i < o1.length; i++) {
+            System.out.println(o1[i] + "  " + o2[i] + "  " + p[i] + "  " + s[i]);
+        }
+    }
+
+    public static String[] getOPattern() {
+        return new String[] {
+            "  *** ",
+            " * * ",
+            "* *",
+            "* *",
+            "* *",
+            " * * ",
+            "  *** "
+        };
+    }
+
+    public static String[] getPPattern() {
+        return new String[] {
+            "***** ",
+            "* * ",
+            "* * ",
+            "***** ",
+            "* ",
+            "* ",
+            "* "
+        };
+    }
+
+    public static String[] getSPattern() {
+        return new String[] {
+            " **** ",
+            "* ",
+            " **** ",
+            "     * ",
+            "     * ",
+            "* * ",
+            " **** "
+        };
+    }
+}
